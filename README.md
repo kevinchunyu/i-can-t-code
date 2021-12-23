0d2e42e78a46e68636f7b52b3d0aec2064681df2
kevinchunyu <kevin11@uw.edu>
2021-12-23 02:40:39 -0800

    added easy questions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # i can't code :)
-leetcode grind
+kevin's leetcode grind
---
+++ easy/PalindromeNumber.java
@@ -0,0 +1,9 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(int x) {
+
+    }
+}
